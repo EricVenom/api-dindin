3a58b53cb4399df1777e46eb2be9aad6b16e5286
Eric <horrorhaus666@gmail.com>
2023-03-01 23:12:17 -0300

    endpoint de cadastro funcionando

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,11 +1,27 @@
+const pool = require('../services/connection');
+const bcrypt = require('bcrypt');
+
 const signUp = async (req, res) => {
     const { nome, email, senha } = req.body;
+
+    if (!nome || !email || !senha) {
+        return res.status(401).json({ mensagem: "Todos os campos são obrigatórios." })
+    }
+
     try {
-        if (!nome || !email || !senha) {
-            return res.json({ mensagem: "Todos os campos são obrigatórios." })
+        const verifyEmail = await pool.query('select * from usuarios where email = $1', [email])
+
+        if (verifyEmail.rowCount) {
+            return res.status(400).json({ mensagem: "Já existe usuário cadastrado com o e-mail informado." });
         }
 
-        return res.json({ nome, email, senha });
+        const encryptedPassword = await bcrypt.hash(senha, 10);
+
+        await pool.query('insert into usuarios (nome, email, senha) values ($1, $2, $3)', [nome, email, encryptedPassword]);
+        const { rows } = await pool.query('select * from usuarios where email = $1', [email])
+        const { senha: _, ...user } = rows[0]
+
+        return res.status(201).json(user);
     } catch (error) {
         return res.status(500).json({ mensagem: "Erro interno do servidor." })
     }
